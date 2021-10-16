10ed64f1d3a3cd1e1e821b26b207126d6f97a0bb
影桜 <79582228+kage-sakura@users.noreply.github.com>
2021-10-17 04:09:59 +0900

    Update CommandAuthor.js

--- src/classes/CommandAuthor.js
+++ src/classes/CommandAuthor.js
@@ -12,6 +12,7 @@ class CommandAuthor extends Base {
 	* @param (CommandInteraction) CommandInteraction
 	 */
 	constructor(client, data = {}, cmdIa = {}) {
+		super();
 		this.id = data.guild_id ? data.member.user.id : data.user.id;
 
 		this.user = client.users.cache.get(this.id);
