6dc6df4a5d3e1feeddce112c7239142c149ee2a6
すずねーう <suzuneu@suzuneu.com>
2021-11-27 09:33:25 +0900

    fix space
    Co-authored-by: 影桜 <79582228+kage-sakura@users.noreply.github.com>

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -44,7 +44,7 @@ export type ApplicationCommandPermissions = {
 
 export type ChannelTypes = "GUILD_TEXT" | "DM" | "GUILD_VOICE" | "GROUP_DM" | "GUILD_CATEGORY" | "GUILD_NEWS" | "GUILD_STORE" | "GUILD_NEWS_THREAD" | "GUILD_PUBLIC_THREAD" | "GUILD_PRIVATE_THREAD" | "GUILD_STAGE_VOICE";
 
-export type	ApplicationCommandOptionsChoices = {
+export type ApplicationCommandOptionsChoices = {
   name: string;
   value: string | number;
 };
